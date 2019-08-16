e0b95d34ce525bcd55e1d19d63773f6155d0cdef
Evgeniy Malyutin <evxn@users.noreply.github.com>
2019-08-16 11:01:15 +0300

    created project

---
+++ radio/Radio/Application/AppDelegate.swift
@@ -0,0 +1,48 @@
+//
+//  AppDelegate.swift
+//  Radio
+//
+//  Created by evg on 15/08/2019.
+//  Copyright © 2019 evgn. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+	 var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+    // MARK: - Core Data
+	
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Radio")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+}
+
---
+++ radio/Radio/Screens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Radio
+//
+//  Created by evg on 15/08/2019.
+//  Copyright © 2019 evgn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
