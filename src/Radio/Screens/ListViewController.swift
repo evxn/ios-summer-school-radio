144e51d1f7227a18e073453bf1e15f018e3a5d5e
Evgeniy Malyutin <evxn@users.noreply.github.com>
2019-08-18 02:08:57 +0300

    added navigation

---
+++ src/Radio/Screens/ListViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Radio
+//
+//  Created by evg on 15/08/2019.
+//  Copyright © 2019 evgn. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ListViewController: UIViewController {
+	
+	let bag = DisposeBag()
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+		
+//		field.rx.text.asDriver()
+//			.drive(label.rx.text)
+//			.disposed(by: bag)
+    }
+
+}
+
